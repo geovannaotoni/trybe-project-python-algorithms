e79bda6f36a127e70fd189d071e725faaaf8ca2d
Geovanna Otoni <geovannaotoni@gmail.com>
2023-10-20 17:48:38 -0300

    feat03: palindromes_recursive

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,3 +1,9 @@
 def is_palindrome_recursive(word, low_index, high_index):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    """Função que verifica se uma palavra é um palíndromo."""
+    if len(word) == 0:
+        return False
+    if low_index >= high_index:
+        return True
+    if word[low_index] != word[high_index]:
+        return False
+    return is_palindrome_recursive(word, low_index + 1, high_index - 1)
