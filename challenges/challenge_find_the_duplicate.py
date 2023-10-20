82d685ec0e751edb954338f19ef692bd8e7031d7
Geovanna Otoni <geovannaotoni@gmail.com>
2023-10-20 20:00:42 -0300

    feat05: find_duplicate

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,3 +1,14 @@
 def find_duplicate(nums):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    """Função que encontra o número que se repete da lista."""
+
+    unique_nums = set()
+    for num in nums:
+        if type(num) is not int or num < 0:
+            return False
+
+        if num in unique_nums:
+            return num
+        else:
+            unique_nums.add(num)
+
+    return False
