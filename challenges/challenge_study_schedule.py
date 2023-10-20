9c2f16543f06e44fb774d35380ab31aa0dee3cfd
Geovanna Otoni <geovannaotoni@gmail.com>
2023-10-20 16:06:39 -0300

    feat02: tests for encrypt_message

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,4 +1,5 @@
 def study_schedule(permanence_period, target_time):
+    """Calcula quantos estudantes estão presentes em um determinado horário."""
     try:
         count = 0
         for start_hour, finish_hour in permanence_period:
--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,13 @@
-# from challenges.challenge_encrypt_message import encrypt_message
+from challenges.challenge_encrypt_message import encrypt_message
+import pytest
 
 
 def test_encrypt_message():
-    pass
+    assert encrypt_message("secret", 2) == "terc_es"
+
+    assert encrypt_message("secret", 3) == "ces_ter"
+
+    assert encrypt_message("secret", 10) == "terces"
+
+    with pytest.raises(TypeError, match="tipo inválido para key"):
+        encrypt_message("secret", "a")
