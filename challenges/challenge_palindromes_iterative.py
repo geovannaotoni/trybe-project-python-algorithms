657526642cd2fc4943ce43e74cb435cee9f63028
Geovanna Otoni <geovannaotoni@gmail.com>
2023-10-20 17:59:35 -0300

    feat06: palindromes_iterative

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,3 +1,15 @@
 def is_palindrome_iterative(word):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    """Função que verifica se uma palavra é um palíndromo."""
+    if len(word) == 0:
+        return False
+
+    # opção 1: com range
+    for i in range(len(word)):
+        if word[i] != word[len(word) - 1 - i]:
+            return False
+    return True
+
+    # opção 2: com slicing
+    # if word != word[::-1]:
+    #     return False
+    # return True
