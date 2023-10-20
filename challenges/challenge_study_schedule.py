8ec9f3b8daace3e54e1ae307ab2b28abee3c1cc8
Geovanna Otoni <geovannaotoni@gmail.com>
2023-10-20 15:45:12 -0300

    feat01: function study_schedule

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,9 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    try:
+        count = 0
+        for start_hour, finish_hour in permanence_period:
+            if start_hour <= target_time <= finish_hour:
+                count += 1
+        return count
+    except TypeError:
+        return None
